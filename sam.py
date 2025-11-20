1bfa9b0e5a4d3839e4e7c7aa8881ea061f0f6aa9
Poorvin123HR <hrpoorvin@gmail.com>
2025-11-20 21:20:35 +0530

    sam.py

--- sam.py
+++ sam.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 # Load your HTML file that contains JS
-with open("example.html", "r", encoding="utf-8") as f:
+with open("1.html", "r", encoding="utf-8") as f:
     html_code = f.read()
 # Render it inside Streamlit
 components.html(html_code, height=600, scrolling=True)
