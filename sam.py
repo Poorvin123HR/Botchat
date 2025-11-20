ded9aec34d588aca6432fdcb3737bb78e7594757
Poorvin123HR <hrpoorvin@gmail.com>
2025-11-20 21:40:12 +0530

    sam.py

--- sam.py
+++ sam.py
@@ -1,7 +1,19 @@
-import streamlit as st
-import streamlit.components.v1 as components
+# import streamlit as st
+#import streamlit.components.v1 as components
 # Load your HTML file that contains JS
-with open("1.html", "r", encoding="utf-8") as f:
+#with open("1.html", "r", encoding="utf-8") as f:
     html_code = f.read()
 # Render it inside Streamlit
 components.html(html_code, height=600, scrolling=True)
+
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Path to your HTML file
+file_path = r"C:\Users\mithu\eclipse-workspace\mini\src\main\webapp\1.html"
+
+with open(file_path, "r", encoding="utf-8") as f:
+    html_content = f.read()
+
+# Render inside Streamlit
+components.html(html_content, height=600, scrolling=True)
